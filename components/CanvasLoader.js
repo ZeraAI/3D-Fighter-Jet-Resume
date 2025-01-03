710db954c36b9d4e8a8252d71afa73e54da13d86
ZeraAI <parkzen68@gmail.com>
2025-01-03 01:11:25 -0600

    Add missing CanvasLoader component

---
+++ components/CanvasLoader.js
@@ -0,0 +1,14 @@
+// components/CanvasLoader.js
+import React from 'react';
+import { Html, useProgress } from '@react-three/drei';
+
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html center>
+      <span>{progress.toFixed(2)}% Loading...</span>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
